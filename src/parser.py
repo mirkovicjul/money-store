f95569ce36d7f8fbb95cc668384324519deb1373
Julija Mirković <mirkovicjul@gmail.com>
2020-07-15 13:39:53 +0200

    Change parser arguments

--- src/parser.py
+++ src/parser.py
@@ -7,17 +7,17 @@ from utils import valid_date
 def transaction_parser(subparser, transaction_type):
     transaction_parser = subparser.add_parser(transaction_type)
 
-    transaction_parser.add_argument("amount", type=int, help="Money amount")
+    transaction_parser.add_argument("amount", type=int, help="Money amount (e.g. -500)")
     transaction_parser.add_argument(
-        "tags", type=str, nargs="+", help="Tag your transactions"
+        "tags", type=str, nargs="+", help="Transaction tags, separated by spaces (e.g. burger beer pub)"
     )
-    transaction_parser.add_argument("--comment", type=str)
+    transaction_parser.add_argument("--comment", type=str, help="Additional information about transaction")
     transaction_parser.add_argument(
         "--date",
-        type=valid_date,
+        type=str,
         required=False,
-        help="Date of transaction - format YYYY-MM-DD",
-        default=datetime.date.today()
+        help="Transaction date in one of the formats: YYYY-MM-DD/YYYY-MM-DD@HH-mm/HH-mm (or just leave empty if transaction is happening now)",
+        default=datetime.now().strftime("%s")
     )
 
 
@@ -30,8 +30,7 @@ def get_parser():
     parser = argparse.ArgumentParser(description="Money store argument parser")
     subparser = parser.add_subparsers(dest="action")
 
-    transaction_parser(subparser, "+")
-    transaction_parser(subparser, "-")
+    transaction_parser(subparser, "tx")
     stats_parser(subparser)
 
     return parser
